1cad1fc6d4afd77891dc69d97098b2cd0d33bca3
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2022-11-02 20:06:45 +0700

    Safety measure for scaffolding

---
+++ ShippingHelper/ShippingHelper.Core/Data/AppDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingHelper.Core.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ShippingProject;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new AppDbContext(optionBuilder.Options);
+        }
+    }
+}
